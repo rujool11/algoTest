2da7ea8d1bc83b31aa117a06d82d274133f340a7
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-02-23 14:49:52 +0530

    add submitProblem API

--- backend/controllers/problems/submitProblem.js
+++ backend/controllers/problems/submitProblem.js
@@ -1,5 +1,44 @@
-const submitProblem = () => {
+import Problem from '../../models/problemModel.js';
+import asyncHandler from 'express-async-handler';
 
-};
+const submitProblem = asyncHandler(async (req, res) => {
+    const { name, description, sampleInput, sampleOutput, difficulty } = req.body;
+
+    if (!name || !description || !sampleInput || !sampleOutput || !difficulty) {   
+        res.status(400);
+        throw new Error("MISSING INPUTS: please fill out all fields");
+    }
+
+    const problemExists = await Problem.findOne({
+        $or: [{ name }]
+    });
+
+    if (problemExists) {
+        res.status(400);
+        throw new Error("NON-UNIQUE INPUTS: problem title already exists");
+    }
+
+    const problem = await Problem.create({
+        name,
+        description,
+        sampleInput,
+        sampleOutput,
+        difficulty,
+    });  
+
+    if (problem) { 
+        res.status(201).json({
+            _id: problem._id,
+            name: problem.name, 
+            description: problem.description, 
+            sampleInput: problem.sampleOutput,
+            difficulty: problem.difficulty,
+        })
+    } else {
+        res.status(400);
+        throw new Error("SERVER ERROR: failed to create problem");
+    }
+}); 
+
 
 export default submitProblem;
--- backend/routes/problemRouter.js
+++ backend/routes/problemRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
-// import protect from '../middlewares/authMiddleware.js';
+import protect from '../middlewares/authMiddleware.js';
 import getAllProblems from '../controllers/problems/getAllProblems.js';
+import submitProblem from '../controllers/problems/submitProblem.js';
 const router = express.Router();
 
 router.get('/', getAllProblems);
-// router.post('/', protect, submitProblem);
+router.post('/', protect, submitProblem);
 
 export default router;
