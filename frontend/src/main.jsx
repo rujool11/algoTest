3abaf2da8e32524fbbc5f13b730401fb84f09980
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-03-09 14:02:21 +0530

    modify Context provider methods

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,35 @@
-import { StrictMode } from "react";
+import { createContext, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AppProvider from "../context/ContextProvider.jsx";
+import { useState, useEffect } from "react";
+
+export const Context = createContext({
+  isAuthorized: false,
+});
+
+const AppWrapper = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [user, setUser] = useState();
+
+  // rehydrate user on initial load
+  useEffect(() => {
+    const storedUser = localStorage.getItem("userInfo");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+      setIsAuthorized(true);
+    }
+  }, []);
+
+  return (
+    <Context.Provider value={{ isAuthorized, setIsAuthorized, user, setUser }}>
+      <App />
+    </Context.Provider>
+  );
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <AppWrapper />
   </StrictMode>
 );
