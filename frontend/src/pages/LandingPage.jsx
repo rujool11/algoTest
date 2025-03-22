5bed43cc181ed61def356883ac100646e8b54079
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-03-22 15:44:59 +0530

    navigate to /problems if user already logged in on initial load

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import Navbar from '../components/misc/Navbar.jsx';
-import Hero from '../components/misc/Hero.jsx';
+import React from "react";
+import Navbar from "../components/misc/Navbar.jsx";
+import Hero from "../components/misc/Hero.jsx";
 import bgImage from "../assets/peakpx.jpg";
+import { Context } from "../main.jsx";
+import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+
+  const { isAuthorized } = useContext(Context);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthorized) {
+      navigate("/problems");
+    }
+  }, [isAuthorized, navigate]);
+
   return (
-    <div 
+    <div
       className="min-h-screen"
       style={{
         backgroundImage: `url(${bgImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-      }}>
+      }}
+    >
       <Navbar />
       <Hero />
       {/* Additional sections */}
