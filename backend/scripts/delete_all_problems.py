130296f6c845826d4938ab7c309d36f56400811f
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-03-23 14:36:36 +0530

    automatically delete all testcases when all problems deleted

--- backend/scripts/delete_all_problems.py
+++ backend/scripts/delete_all_problems.py
@@ -12,10 +12,16 @@ client = MongoClient(MONGO_URI)
 db = client['test']
 
 problems_collection = db['problems']
+testcase_collection = db['testcases']
 
 def delete_all_problems():
     result = problems_collection.delete_many({})
     print(f"Deleted {result.deleted_count} documents from the 'problems' collection.")
 
+def delete_all_testcases():
+    result = testcase_collection.delete_many({})
+    print(f"Deleted {result.deleted_count} documents from the 'testcases' collection.")
+
 if __name__ == "__main__":
     delete_all_problems()
+    delete_all_testcases()
