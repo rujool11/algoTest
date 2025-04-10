3e9038a0b8350c720f8d72a1849585231b228f38
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-04-10 14:58:40 +0530

    add run routes

---
+++ backend/controllers/run/runProblem.js
@@ -0,0 +1,5 @@
+const runProblem = () => {
+
+}
+
+export default runProblem;
--- backend/routes/runRouter.js
+++ backend/routes/runRouter.js
@@ -1,4 +1,9 @@
 import express from 'express'
+import runProblem from '../controllers/run/runProblem.js';
+
 
 const router = express.Router();
+
+router.post('/', runProblem);
+
 export default router;
